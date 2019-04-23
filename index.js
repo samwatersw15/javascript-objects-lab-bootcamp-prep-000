298200ec7da9a1cb01487b4f9485a4d6d85237a3
Sam Waters <samwatersw15@gmail.com>
2019-04-23 19:46:01 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {
+  'tomatoes':4,
+  'lettuce':'iceberg',
+  'lemon juice':'sprinkle',
+  'red onion':'chopped'
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+  return object
+}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  
+}
