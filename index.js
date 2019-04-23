a371d168904fa35fbf65cf4c2f5c4c7447292cd2
Sam Waters <samwatersw15@gmail.com>
2019-04-23 19:50:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function destructivelyDeleteFromObjectByKey(object, key) {
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object,{key:[value]})
+  return Object.assign({}, object,{ key: value })
 }
